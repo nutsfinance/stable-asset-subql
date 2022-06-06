7e2a5e762e9c801136ec3087597dd974e0db9f09
Shengda Ding <dingshengda@gmail.com>
2022-06-06 11:07:58 -0700

    Fix casting

--- src/utils/pool.ts
+++ src/utils/pool.ts
@@ -10,19 +10,19 @@ export interface PoolInfo {
 }
 
 export const getPoolDecimals = async (poolId: number) => {
-    const poolInfo = (await api.query.stableAsset.pools(poolId) as Option<Codec>).unwrap();
+    const poolInfo = (await api.query.stableAsset.pools(poolId) as unknown as Option<Codec>).unwrap();
     return Math.log10((poolInfo as any).precision);
 }
 
 export const getPoolInfo = async (poolId: number): Promise<PoolInfo> => {
-    const poolInfo = (await api.query.stableAsset.pools(poolId) as Option<Codec>).toJSON() as any;
+    const poolInfo = (await api.query.stableAsset.pools(poolId) as unknown as Option<Codec>).toJSON() as any;
     const decimals = Math.log10(poolInfo.precision);
 
     const totalSupply = getNumber(poolInfo.totalSupply, decimals);
     const balances = poolInfo.balances.map(balance => getNumber(balance, decimals));
 
-    const feeAccount = (await api.query.tokens.accounts(poolInfo.feeRecipient.toString(), { StableAssetPoolToken: poolId }) as Option<Codec>).toJSON() as any;
-    const yieldAccount = (await api.query.tokens.accounts(poolInfo.yieldRecipient.toString(), { StableAssetPoolToken: poolId }) as Option<Codec>).toJSON() as any;
+    const feeAccount = (await api.query.tokens.accounts(poolInfo.feeRecipient.toString(), { StableAssetPoolToken: poolId }) as unknown as Option<Codec>).toJSON() as any;
+    const yieldAccount = (await api.query.tokens.accounts(poolInfo.yieldRecipient.toString(), { StableAssetPoolToken: poolId }) as unknown as Option<Codec>).toJSON() as any;
     const feeBalance = getNumber(feeAccount.free, decimals);
     const yieldBalance = getNumber(yieldAccount.free, decimals);
 
